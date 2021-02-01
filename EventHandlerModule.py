8e8c3ff3e236d38197684dad915f251ce5b02ed9
XiaNight <45822697+XiaNight@users.noreply.github.com>
2021-02-01 01:09:52 -0800

    Add files via upload

---
+++ EventHandlerModule.py
@@ -0,0 +1,6 @@
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+
+class SectionSetting:
+    pass
---
+++ app.py
@@ -0,0 +1,268 @@
+# heroku login
+# heroku git:remote -a xia-anti-noob
+# git init
+# git add .
+# git commit -am'ok'
+# git push heroku master
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+
+#======這裡是呼叫的檔案內容=====
+from message import *
+from new import *
+from Function import *
+#======這裡是呼叫的檔案內容=====
+
+#======python的函數庫==========
+import tempfile, os
+import datetime
+import json
+import time
+import os
+#======python的函數庫==========
+
+app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi('cwBEnoA7OI09xMRwZj2JGgVLTiCY8h2fraLjFmwDMHx+JXKuewOBE5eh6xUCIt+1VQOoPVmUlow5xkZDY1oPY7yYPFcd9rN2JqtwCGH3X9Q59VnjPuC4dOqgvXpfW9P3JOgAjgkg+kVFh8yl4wEBJAdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('613e26ffd74fdb430ae58f71af00eedc')
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+class EventHandler:
+
+    #Event
+    event = None
+
+    sourceID = None
+
+    debugMode = True
+
+    def __init__(self):
+        pass
+
+    def OnEventGet(self, event):
+        self.event = event
+
+        try:
+            self.GetSource()
+
+            if event.type == 'message':
+                self.MessageEvent()
+            if event.type == 'memberJoined':
+                self.MemberJoinEvent()
+            if event.type == 'memberLeft':
+                self.MemberLeaveEvent()
+            if event.type == 'join':
+                self.JoinEvent()
+            # self.userID = event.source.user_id
+            # self.userProfile = line_bot_api.get_profile(self.userID)
+
+            if self.debugMode:
+                self.Debug()
+
+            self.Default()
+
+        except Exception as e:
+            self.Print(str(e))
+
+    def MessageEvent(self):
+        msg = self.event.message.text
+        if msg[0] == '#':
+            for command in msg.split("#"):
+                exec(command)
+        elif msg[0] == '$':
+            command = msg[1:]
+            if command == 'help':
+                self.Print('$EnableDebug() to enable debug\n$DisableDebug() to disable debug\nstart with # to execute raw python code.')
+            if command == 'EnableDebug':
+                self.EnableDebug()
+            if command == 'DisableDebug':
+                self.DisableDebug()
+
+    def GetSource(self):
+        if self.event.source.type == 'user':
+            self.sourceID = self.event.source.user_id
+        elif self.event.source.type == 'group':
+            self.sourceID = self.event.source.group_id
+        elif self.event.source.type == 'room':
+            self.sourceID = self.event.source.room_id
+    
+    def MemberJoinEvent(self):
+        self.Print('Member Joined')
+
+        userProfile = self.event.joined.members[0]
+
+        self.Print('User ID: ' + userProfile.user_id)
+
+        profile = line_bot_api.get_profile(userProfile.user_id)
+
+        self.Print('User Display Name: ' + profile.display_name)
+        self.Print('User ID: ' + profile.user_id)
+        self.Print('User Picture URL: ' + profile.picture_url)
+        self.Print('User Status Message: ' + profile.status_message)
+
+        pass
+
+    def MemberLeaveEvent(self):
+        self.Print('Member Left')
+
+        userProfile = self.event.left.members[0]
+
+        self.Print('User ID: ' + userProfile.user_id)
+
+        profile = line_bot_api.get_profile(userProfile.user_id)
+        
+        recallMessage = 'You have left the group: rejoin the group by entering this website: https://line.me/R/ti/g/IS1dFUCScA'
+
+        line_bot_api.push_message(userProfile.user_id, TextSendMessage(text=recallMessage))
+
+        self.Print('User Display Name: ' + profile.display_name)
+        self.Print('User ID: ' + profile.user_id)
+        self.Print('User Picture URL: ' + profile.picture_url)
+        self.Print('User Status Message: ' + profile.status_message)
+        pass
+
+    def JoinEvent(self): # When bot joins a room/group.
+        self.Print('Bot Joined')
+        pass
+
+    def FollowEvent(self): # When user added bot as friend.
+        pass
+
+    def Default(self):
+        pass
+
+    def PrintUserProfile(self):
+        pass
+
+    def Print(self, text):
+        message = TextSendMessage(text=text)
+        line_bot_api.push_message(self.sourceID, message)
+
+    def Debug(self):
+        self.Print(str(self.event))
+    def EnableDebug(self):
+        self.Print('Debug Enabled')
+        self.debugMode = True
+    def DisableDebug(self):
+        self.Print('Debug Disabled')
+        self.debugMode = False
+
+EH = EventHandler()
+
+@handler.add(MemberJoinedEvent)
+def handle_member_joined(event):
+    EH.OnEventGet(event)
+
+@handler.add(MemberLeftEvent)
+def handle_member_left(event):
+    EH.OnEventGet(event)
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    EH.OnEventGet(event)
+
+    msg = event.message.text
+    if '最新合作廠商' in msg:
+        message = imagemap_message()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif '最新活動訊息' in msg:
+        message = buttons_message()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif '註冊會員' in msg:
+        message = Confirm_Template()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif '旋轉木馬' in msg:
+        message = Carousel_Template()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif '圖片畫廊' in msg:
+        message = test()
+        line_bot_api.reply_message(event.reply_token, message)
+    elif '功能列表' in msg:
+        message = function_list()
+        line_bot_api.reply_message(event.reply_token, message)
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    EH.OnEventGet(event)
+
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+    EH.OnEventGet(event)
+
+@handler.add(JoinEvent)
+def handle_join(event):
+    EH.OnEventGet(event)
+
+@handler.add(LeaveEvent)
+def handle_leave():
+    EH.OnEventGet(event)
+
+def SendTextMessage(replyToken, text):
+    message = TextSendMessage(text=text)
+    line_bot_api.reply_message(replyToken, message)
+
+# return (successfullness, error message)
+def SaveSectionJson(sourceID, key, value):
+
+    if not os.path.exists('group_data'):
+        os.makedirs('group_data')
+
+    path = 'group_data/' + sourceID + '.json'
+    data = None
+
+    if not os.path.exists(path):
+        with open(path, 'w') as outfile:
+            outfile.write('{}')
+
+    try:
+        with open(path) as json_file:
+            data = json.load(json_file)
+            data[key] = value
+    except Exception as e:
+        pass
+    
+
+    with open(path, 'w') as outfile:
+        json.dump(data, outfile)
+
+def ReadSectionJson(sourceID):
+    path = 'group_data/' + sourceID + '.json'
+    with open(path) as json_file:
+        data = FromJson(json_file)
+        return data
+
+def FromJson(text):
+    exec('return ' + text)
+
+def ToJson(text):
+    return str(text)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
