0cf666c4b5d5416b22e23f1e9772d63facf73a9a
XiaNight <XiaNight@users.noreply.github.com>
2021-06-27 15:30:30 +0800

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -14,15 +14,13 @@ import discord
 from discord.ext import commands
 
 def DiscordBot():
-
     bot = commands.Bot(command_prefix='.')
     XSF = XStandFor()
     GS = GoogldSheet()
 
     print('running discord bot!')
     token = os.getenv("DISCORD_BOT_TOKEN")
-    bot.run(token)
-
+    
     @bot.command()
     async def ping(ctx):
         await ctx.send('pong')
@@ -38,7 +36,9 @@ def DiscordBot():
         trashes = GS.GetRange('TrashTalk!A:A', majorDimension='COLUMNS')[0]
         randomInt = random.randint(0, len(trashes) - 1)
         await ctx.send(trashes[randomInt])
-        
+
     @bot.event
     async def on_ready():
         print('Bot is ready.')
+    
+    bot.run(token)
